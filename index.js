a2d472aa8baed669623616a5b0d039df12289355
Sivan Gilead <sivan.gilad@gmail.com>
2018-09-01 16:16:56 -0400

    fix diagonal

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 window.onload = function () {
     document.getElementsByTagName('input')[0].focus();
+    const arrow = document.getElementsByClassName('arrowContainer')[0]
+    arrow.addEventListener("click", listener, false);
+
+    function listener() {
+        var timerID = setInterval(function () {
+            window.scrollBy(0, 5);
+
+            if (window.pageYOffset >= 700)
+                clearInterval(timerID);
+        }, 13);
+
+    }
 }
+
